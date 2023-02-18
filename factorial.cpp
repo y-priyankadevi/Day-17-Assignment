b18ea7f75e745790131560680a2e43e8dfd01275
y-priyankadevi <125555432+y-priyankadevi@users.noreply.github.com>
2023-02-18 08:54:54 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int i,fact=1;
+ 	printf("enter i");
+ 	scanf("%d",&i);
+ 	while(i>=1)
+ {
+ 	fact=fact*i;
+ 	
+ }
+
+  printf("the factorial of a given number is %d");
+  return 0;
+}
+	
+ 
